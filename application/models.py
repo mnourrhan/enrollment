7f95a05f7eb1231f8e44904eef801395b71b7323
mnourrhan <mnourrhan@gmail.com>
2019-12-02 16:30:12 +0200

    Work Done: ------------- - Integrate with flask-security. - Integrate with flask forms. - Create form for login and register with validations. - Configure a generated secret key. - Handle user login using form and session. - Handle user register using form and sessions. - Hide login and register tabs if user already signed in. - Add logout feature. - Display all the existing courses from course table not using a static data. - Handle preforming the enrollment action and creating the relation between user and the enrolled course. - Add Enrollment tab and display all the enrolled courses for the loggin in user inside it. - Use werkzeug.security for hashing user's password.

--- application/models.py
+++ application/models.py
@@ -1,16 +1,24 @@
 import flask
 from application import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Document):
-    user_id = db.IntField( unique=True )
+    user_id = db.IntField()
     first_name = db.StringField( max_length=50 )
     last_name = db.StringField( max_length=50 )
     email = db.StringField( max_length=30 )
-    password = db.StringField( max_length=30 )
+    password = db.StringField()
+    
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+    
+    def get_password(self, password):
+        return check_password_hash(self.password, password)
+        
 
-class course(db.Document):
-    course_id = db.StringField( max_length=10, unique=True )
+class Course(db.Document):
+    courseID = db.StringField( max_length=10, unique=True )
     title = db.StringField( max_length=100 )
     description = db.StringField( max_length=255 )
     credits = db.IntField()
@@ -19,4 +27,4 @@ class course(db.Document):
 # The many to many relationship table between course and user
 class Enrollment(db.Document):
     user_id = db.IntField()
-    course_id = db.StringField( max_length=10 )
+    courseID = db.StringField( max_length=10 )
--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret_string'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or b'\xa8\xa7\xf6\x83o\xd1\xe3zkI}j\xc5\x96X\x97'
 
     MONGODB_SETTINGS={
         'db' : 'UTA_Enrollement'
