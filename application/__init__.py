67a4cad8f3e6a833b4fcfd1b5fe015d650be590b
mnourrhan <mnourrhan@gmail.com>
2019-12-02 17:55:56 +0200

    Work Done: ------------ - Integrate with flask-restplus and configure it. - Create api for getting all users and a specific user through GET request. - Create api for updating user data through PUt request. - Create api for deleting user through DELETE request. - Create api for creating new user through PUT request.

--- application/__init__.py
+++ application/__init__.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, Blueprint
 from config import Config
 from flask_mongoengine import MongoEngine
+from flask_restplus import Api
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = MongoEngine()
 db.init_app(app)
+
+blueprint = Blueprint('api',__name__,url_prefix='/restful')
+api = Api(blueprint, doc='/doc/')
+app.register_blueprint(blueprint)
+# api.init_app(app)
 
 from application import routes
